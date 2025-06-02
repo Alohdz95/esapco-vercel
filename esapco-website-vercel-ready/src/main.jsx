c58a98f9ca6552122d003d0fb7f2850e5b587be4
Alohdz95 <jalo9608@gmail.com>
2025-06-02 09:38:41 -0600

    Add files via upload

---
+++ esapco-website-vercel-ready/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <h1>ESAPCO Software</h1>
+    <p>¡Sitio listo para publicar en Vercel!</p>
+  </React.StrictMode>
+)
